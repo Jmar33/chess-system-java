b66417168a66fef6d96c1ca9dc7803d0e2e50a0b
Jmar33 <josimarferreira922@yahoo.com.br>
2024-09-14 11:32:18 -0300

    Implementing possible moves of Rook

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,7 +19,7 @@ public class Program {
 		
 		while(true) {
 			try {
-				UI.clearScreen();
+				//UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces());
 				System.out.println();
 				System.out.print("Source: ");
